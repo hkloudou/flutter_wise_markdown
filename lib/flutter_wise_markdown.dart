dd23eec5acd314d862e891f7f9eecc5667d90ba6
hkloudou <hkloudou@gmail.com>
2021-06-09 00:44:58 +0800

    auto commit

--- lib/flutter_wise_markdown.dart
+++ lib/flutter_wise_markdown.dart
@@ -1,7 +1,14 @@
 library flutter_wise_markdown;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'dart:ui';
+import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:markdown/markdown.dart' as md;
+
+import 'package:url_launcher/url_launcher.dart';
+import 'dart:io';
+part 'src/router/router.dart';
+part 'src/router/MarkDownPage.dart';
+part 'src/router/LaunchAdapter.dart';
+part 'src/router/WiseLaunchAdapter.dart';
